09df70125e6736447ba88bb6c90d70379de9b300
Kaita Nakamura <kaita.nakamura0830@gmail.com>
2025-08-06 10:13:45 +0900

    feat
    Signed-off-by: Kaita Nakamura <kaita.nakamura0830@gmail.com>

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,11 +1,11 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 21.0"
 
-  cluster_name    = var.name
-  cluster_version = "1.33"
+  name               = var.name
+  kubernetes_version = "1.33"
 
-  cluster_addons = {
+  addons = {
     coredns                = {}
     kube-proxy             = {}
     vpc-cni                = {}
@@ -15,20 +15,21 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access       = true
-  cluster_endpoint_private_access      = true
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  endpoint_private_access      = true
+  endpoint_public_access       = true
+  endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   enable_cluster_creator_admin_permissions = true
 
   self_managed_node_groups = {
     default = {
       ami_type      = "AL2023_x86_64_STANDARD"
+      ami_id        = "ami-0077d3b140075152e"
       instance_type = "m6i.large"
 
-      min_size     = 2
-      max_size     = 2
-      desired_size = 2
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
 
       # iam_role_additional_policies = {
       #   AmazonS3ReadOnlyAccess = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
